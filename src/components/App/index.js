cf815913b88af0f2fe9e92c4ddae2330526538af
rishabhrao <rishabhraos1@gmail.com>
2020-09-19 09:21:29 +0530

    👉 Cleanup 🔥

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+/*Copyright © 2020 Rishabh Rao.
+All Rights Reserved.*/
+
+import React from "react";
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
